6757e9e333a1b2d63de57871b0e0950fd63ae6e7
David Tejada <davidxtejada@gmail.com>
2022-03-21 11:58:11 +0100

    Convert newTodo to function component

--- src/components/newTodo.jsx
+++ src/components/newTodo.jsx
@@ -1,39 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default class NewTodo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: "" };
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
+export default function NewTodo({ onNewTodo }) {
+  const [text, setText] = useState("");
+
+  function handleTextChange(e) {
+    setText(e.target.value);
   }
 
-  handleTextChange(event) {
-    this.setState({ text: event.target.value });
-  }
-
-  onFormSubmit(event) {
-    event.preventDefault();
-    this.props.onNewTodo({
-      content: this.state.text,
+  function onFormSubmit(e) {
+    e.preventDefault();
+    onNewTodo({
+      content: text,
       id: uuidv4(),
       completed: false,
     });
-    this.setState({ text: "" });
+    setText("");
   }
 
-  render() {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <input
-          type="text"
-          placeholder="Enter new todo..."
-          onChange={this.handleTextChange}
-          value={this.state.text}
-        />
-        <button type="submit">+</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter new todo..."
+        onChange={handleTextChange}
+        value={text}
+      />
+      <button type="submit">+</button>
+    </form>
+  );
 }
